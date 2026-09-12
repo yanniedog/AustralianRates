432c7887c8c75273c30dfb621425bdf2550ba8bf
yanniedog <jkokavec@gmail.com>
2026-09-12 14:27:52 +1000

    Remove local-manual-CDR-ingest (standalone AR-local repo) (#337)
    * Fix local CDR dashboard hierarchy and rates
    
    * Remove local-manual-CDR-ingest; tooling moved to standalone repo c:\code\AR-local.

--- local-manual-CDR-ingest/START_HERE.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0START_HERE.ps1"
-if errorlevel 1 pause
--- local-manual-CDR-ingest/rebuild_exports.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0START_HERE.ps1" -Action rebuild
-echo.
-pause
